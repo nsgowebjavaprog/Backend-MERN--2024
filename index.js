610816c342e76481cd7af19f0dc8b0da7c5b5989
NS258 <154657596+nsgowebjavaprog@users.noreply.github.com>
2024-11-11 19:01:30 +0530

    Add files via upload
    addes

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const fs = require('fs');
+// Set up view engine
+app.set('view engine', 'ejs');
 
-app.get("/", function(req, res){
-    res.send("Hello NS LONI");
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route setup
+app.get('/', function(req, res){
+   fstat.readdir(`./files`, function(err, files){
+    res.render("index", {files: files});
+   })
+})
+//store file name = df df df ==> dfdfdf.txt
+app.get('/create', function(req, res){
+    fstat.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/");
+    })
 })
 
-app.get("/profile", function(req, res){
-    res.send("It's NS LONI Profile")
-})
-
-app.listen(3000, () => {
-    console.log(`Server listening on port 3000`);
-});
+app.listen(3000);
